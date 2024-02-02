2a5e1cd5cbff569ffb16cbbdef88cacf7994ed29
OliverYR03 <oliver_del22@hotmail.com>
2024-02-02 14:47:40 -0500

    User-request-for-login

---
+++ src/main/java/com/pe/zenkai/AJMInventario/Controller/UserController.java
@@ -0,0 +1,31 @@
+package com.pe.zenkai.AJMInventario.Controller;
+
+import com.pe.zenkai.AJMInventario.User.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Integer id)
+    {
+        UserDTO userDTO = userService.getUser(id);
+        if (userDTO==null)
+        {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userDTO);
+    }
+
+    @PutMapping()
+    public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest userRequest)
+    {
+        return ResponseEntity.ok(userService.updateUser(userRequest));
+    }
+}
